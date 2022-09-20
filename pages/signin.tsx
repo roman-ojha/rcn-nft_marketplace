758a4b8acdff8b6656c8df908af1fcef0be0347b
Roman-Ojha <razzroman98@gmail.com>
2022-09-20 18:31:31 +0545

    feat: completed creating & styling singin page

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -2,17 +2,30 @@ import type { NextPage } from "next";
 import Image from "next/image";
 import styles from "../styles/pages/signin.module.scss";
 import appIcon from "../assets/images/appIcon.png";
+import { Icon } from "@iconify/react";
+import loginImage from "../assets/images/login.png";
 
 const Signin: NextPage = () => {
   return (
     <>
       <div className={styles.container}>
-        <div className={styles["left-part"]}>
-          <div className={styles["left-part-inner-component"]}>
-            <div className={styles["app-icon"]}>
-              <Image src={appIcon} alt="login" />
+        <div className={styles.left_part}>
+          <div className={styles.left_part_inner_component}>
+            <div className={styles.app_icon}>
+              <Image src={appIcon} alt="RCN" />
             </div>
-            <h1>Sign In</h1>
+            <div className={styles.sign_in_container}>
+              <h1>Sign In</h1>
+              <div className={styles.sign_in_container_button_container}>
+                <Icon icon="logos:metamask-icon" />
+                <h2>Connect Wallet</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.right_part}>
+          <div className={styles.login_image}>
+            <Image src={loginImage} alt="login" />
           </div>
         </div>
       </div>
