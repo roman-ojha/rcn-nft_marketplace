226a5ace2ba7aa8509be9b07340f85cbbda59d5f
Roman-Ojha <razzroman98@gmail.com>
2022-09-20 17:05:55 +0545

    style(sass): create function to get-color

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from "next";
+import styles from "../styles/pages/signin.module.scss";
 
 const Signin: NextPage = () => {
   return (
     <>
-      <div></div>
+      <div className={styles.container}></div>
     </>
   );
 };
