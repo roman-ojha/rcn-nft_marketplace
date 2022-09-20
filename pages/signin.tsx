643b1304006748eaa64d51b9940162651dc7fc6c
Roman-Ojha <razzroman98@gmail.com>
2022-09-20 17:59:36 +0545

    style: func(get-font-family) & designing left-part login page

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,10 +1,21 @@
 import type { NextPage } from "next";
+import Image from "next/image";
 import styles from "../styles/pages/signin.module.scss";
+import appIcon from "../assets/images/appIcon.png";
 
 const Signin: NextPage = () => {
   return (
     <>
-      <div className={styles.container}></div>
+      <div className={styles.container}>
+        <div className={styles["left-part"]}>
+          <div className={styles["left-part-inner-component"]}>
+            <div className={styles["app-icon"]}>
+              <Image src={appIcon} alt="login" />
+            </div>
+            <h1>Sign In</h1>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
