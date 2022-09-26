6cb890434bd8afc6bab6ca85780aca84d6932341
Roman-Ojha <razzroman98@gmail.com>
2022-09-26 16:48:07 +0545

    style: designed sidebar

--- components/Sidebar/Index.tsx
+++ components/Sidebar/Index.tsx
@@ -2,7 +2,17 @@ import type { NextPage } from "next";
 import styles from "../../styles/components/sidebar.module.scss";
 
 const Index: NextPage = () => {
-  return <aside className={styles.container}></aside>;
+  return (
+    <aside className={styles.container}>
+      <ul className={styles.main}>
+        <li className={styles.main__trending}>Trending</li>
+        <li className={styles.main__top}>Top</li>
+        <li className={styles.main__new}>New</li>
+        <li className={styles.main__collection}>Collection</li>
+        <li className={styles.main__favorite}>Favorite</li>
+      </ul>
+    </aside>
+  );
 };
 
 export default Index;
